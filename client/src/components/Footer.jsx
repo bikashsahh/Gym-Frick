d147fff54dc2993fad0f9f66f6f1038d2ac5cad3
bikashsahh <bikashsah0312@email.com>
2024-06-13 16:17:00 +0530

    restyle

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,25 +1,11 @@
-// import React from 'react';
-// import { Box, Stack, Typography } from '@mui/material';
-
-// const Footer = () => (
-  //   <Box mt="40px" bgcolor="white">
-  //     <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
-  // <img src={Logo} alt="logo" style={{ width: '200px', height: '60px' }} />
-  //     </Stack>
-  //     <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made with ❤️ by Bikash</Typography>
-  //   </Box>
-  // );
-  
-  // export default Footer;
-  import React from 'react';
-  import styled from 'styled-components';
-  // import Logo from '../assets/images/gym-logo.png';
+import React from "react";
+import styled from "styled-components";
 
 const FooterContainer = styled.footer`
   text-align: center;
   padding: 10px;
   margin-top: 100px;
-  flexWrap :wrap
+  flexwrap: wrap;
 `;
 
 const FooterText = styled.p`
@@ -43,7 +29,7 @@ const Footer = () => {
   return (
     <FooterContainer>
       <FooterText>
-      {/* <img src={Logo} alt="logo" style={{ width: '100px', height: '60px' ,marginBottom:'-20px'}} /> */}
+        {/* <img src={Logo} alt="logo" style={{ width: '100px', height: '60px' ,marginBottom:'-20px'}} /> */}
         <span>&copy; {currentYear} </span>
         Made with <HeartIcon>❤️</HeartIcon> by Bikash.
       </FooterText>
